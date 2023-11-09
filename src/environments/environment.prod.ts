91e88306b5dba10b6190e155b35bbdc7b11c3911
Andrew Gould <andrew.scott.gould@gmail.com>
2023-11-08 23:05:42 -0500

    update configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,9 +4,9 @@ export const environment = {
   production: false,
   api: {
     domain: 'bcmx.org',
-    base: 'https://www.bcmx.org/',
-    baseUrl: 'https://www.bcmx.org/api/',
-    token: 'https://www.bcmx.org/api/token/refresh',
+    base: 'https://bcmx.org/',
+    baseUrl: 'https://bcmx.org/api/',
+    token: 'https://bcmx.org/api/token/refresh',
   },
   discordSignin:
     'https://discord.com/api/oauth2/authorize?client_id=1163210249864495274&redirect_uri=https%3A%2F%2Fbcmx.org%2Fprofile&response_type=token&scope=identify%20guilds%20guilds.join%20guilds.members.read',
