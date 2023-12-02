279206e05ac9f78cbb3cfe3107b80750212736f6
Nick Alt <altnp291@gmail.com>
2023-12-02 15:26:05 -0500

    update prod urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
   api: {
     domain: 'bcmx.org',
     base: 'https://bcmx.org/',
-    baseUrl: 'https://bcmx.org/api/',
-    token: 'https://bcmx.org/api/token/refresh',
+    baseUrl: 'https://api.bcmx.org/',
+    token: 'https://api.bcmx.org/token/refresh',
   },
   discordSignin:
     'https://discord.com/api/oauth2/authorize?client_id=1163210249864495274&redirect_uri=https%3A%2F%2Fbcmx.org%2Fprofile&response_type=token&scope=identify%20guilds%20guilds.join%20guilds.members.read',
