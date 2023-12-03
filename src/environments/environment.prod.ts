dd5b2b3c03217afc4d810d00ad6f60e0179275ba
Andrew Gould <andrew.scott.gould@gmail.com>
2023-12-03 13:15:55 -0500

    update discord auth url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,5 +9,5 @@ export const environment = {
     token: 'https://api.bcmx.org/token/refresh',
   },
   discordSignin:
-    'https://discord.com/api/oauth2/authorize?client_id=1163210249864495274&redirect_uri=https%3A%2F%2Fbcmx.org%2Fprofile&response_type=token&scope=identify%20guilds%20guilds.join%20guilds.members.read',
+    'https://discord.com/api/oauth2/authorize?client_id=1163210249864495274&response_type=token&redirect_uri=https%3A%2F%2Fbcmx.org%2Fprofile&scope=identify+guilds+guilds.join+guilds.members.read',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,5 +7,6 @@ export const environment = {
     token: 'http://localhost:4300/token/refresh',
   },
   discordSignin:
-    'https://discord.com/api/oauth2/authorize?client_id=1163210249864495274&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fprofile&response_type=token&scope=identify%20guilds%20guilds.join%20guilds.members.read',
+    //'https://discord.com/api/oauth2/authorize?client_id=1163210249864495274&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fprofile&response_type=token&scope=identify%20guilds%20guilds.join%20guilds.members.read',
+    'https://discord.com/api/oauth2/authorize?client_id=1163210249864495274&response_type=token&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fprofile&scope=identify+guilds+guilds.join+guilds.members.read',
 };
