a9e8f5a9815da7fd44b50757f1d6ac1abb6d5709
Andrew Gould <andrew.scott.gould@gmail.com>
2023-12-04 23:54:21 -0500

    TOKENS NOT CODES

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,5 +10,5 @@ export const environment = {
   },
   oxblApiKey: 'eb9ab783-e41f-d428-0319-fb2d4d4a0d71',
   discordSignin:
-    'https://discord.com/api/oauth2/authorize?client_id=1163210249864495274&response_type=code&redirect_uri=https%3A%2F%2Fwww.bcmx.org%2Fprofile&scope=identify+guilds+guilds.join+guilds.members.read',
+    'https://discord.com/api/oauth2/authorize?client_id=1163210249864495274&response_type=token&redirect_uri=https%3A%2F%2Fwww.bcmx.org%2Fprofile&scope=identify+guilds+guilds.join+guilds.members.read',
 };
